33e4f9f277554956bcda92b2bb8024d9d4c5a7cf
Dev Aze <azevchk@gmail.com>
2025-03-25 00:02:21 -0300

    new events and commands

--- main.py
+++ main.py
@@ -14,4 +14,23 @@ bot = commands.Bot(command_prefix=".", intents=intents)
 async def on_ready():
     print("Bot is ready")
 
+@bot.event()
+async def on_member_join(membro:discord.member):
+
+@bot.command()
+async def ola(ctx:commands.Context, *,texto):
+    nome = ctx.author.name
+    await ctx.reply(f"Olá, {nome}! Tudo bem?")
+
+@bot.command()
+async def ola(ctx:commands.Context, texto):
+    await ctx.send(texto)
+
+@bot.command()
+async def somar(ctx:commands.Context, num1:float, num2:float):
+    resultado = num1 + num2
+    await ctx.send(f"a soma entre {num1} e {num2} é igual a {resultado}")    
+
+
+
 bot.run(TOKEN)
